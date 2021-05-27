60bec9f9a8fb96e52a4f237e43df89b7dbadc7a8
redfish1110 <otj18.4978z572m@icloud.com>
2021-05-27 14:34:32 +0900

    いいね非同期機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery
-//= require rails-ujs
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_prototypes, through: :likes, source: :prototype
- 
 end
 
